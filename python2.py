0894495d3c546f1bb38c11d0190ac8effe740899
Ajayalla <107462438+Ajayalla@users.noreply.github.com>
2023-10-26 11:44:21 +0530

    Update and rename python2 to python2.py

--- python2.py
+++ python2.py
@@ -1,7 +1,28 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 import random
 import time
 
-# ... Other code ...
+# Using the Python Device SDK for IoT Hub:
+#   https://github.com/Azure/azure-iot-sdk-python
+# The sample connects to a device-specific MQTT endpoint on your IoT Hub.
+from azure.iot.device import IoTHubDeviceClient, Message
+
+# The device connection string to authenticate the device with your IoT hub.
+# Using the Azure CLI:
+# az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
+CONNECTION_STRING = "HostName=iothubd.azure-devices.net;DeviceId=iotdevice1;SharedAccessKey=QvW3OeMzGwpnTg4o1ytfCqWbJLcGHZ/SAAIoTMWklZg="
+
+# Define the JSON message to send to IoT Hub.
+TEMPERATURE = 20.0
+HUMIDITY = 60
+MSG_TXT = '{{"temperature": {temperature},"humidity": {humidity}}}'
+
+def iothub_client_init():
+    # Create an IoT Hub client
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+    return client
 
 def iothub_client_telemetry_sample_run():
     try:
